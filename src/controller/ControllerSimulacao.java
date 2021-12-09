02b168c04805e3b5ed12a5d412a4ef780a170fbb
Eloísa Bazzanella <elobazzanella@gmail.com>
2021-12-09 17:08:30 -0300

    Reposição de Carros

--- src/controller/ControllerSimulacao.java
+++ src/controller/ControllerSimulacao.java
@@ -7,6 +7,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import model.Veiculo;
 import model.Pista;
@@ -15,25 +17,28 @@ import view.tablemodel.TableModelMalha;
 
 /**
  * Classe de Controller da Simulação
+ *
  * @author Eloísa Bazzanella e Maria Eduarda Buzana
- * @since  novembro, 2021
+ * @since novembro, 2021
  */
 public class ControllerSimulacao implements InterfaceControllerObserved {
-     
+
+    private boolean start = true;
+
     private Pista[][] malha;
-    
+
     private ArrayList<Pista> pistasEntrada;
 
     private ArrayList<Veiculo> veiculos;
-    
+
     private ArrayList<InterfaceViewObserver> observers;
-       
+
     private static ControllerSimulacao instance = null;
 
     public ControllerSimulacao() {
-        this.observers       = new ArrayList<InterfaceViewObserver>();
-        this.veiculos          = new ArrayList<Veiculo>();
-        this.pistasEntrada   = new ArrayList<Pista>();
+        this.observers = new ArrayList<InterfaceViewObserver>();
+        this.veiculos = new ArrayList<Veiculo>();
+        this.pistasEntrada = new ArrayList<Pista>();
     }
 
     public static ControllerSimulacao getInstance() {
@@ -42,7 +47,7 @@ public class ControllerSimulacao implements InterfaceControllerObserved {
         }
         return instance;
     }
-    
+
     public ArrayList<Pista> getPistasEntrada() {
         return pistasEntrada;
     }
@@ -58,7 +63,11 @@ public class ControllerSimulacao implements InterfaceControllerObserved {
     public void addVeiculo(Veiculo carro) {
         this.veiculos.add(carro);
     }
-    
+
+    public void removeVeiculo(Veiculo carro) {
+        this.veiculos.remove(carro);
+    }
+
     public void addObserver(InterfaceViewObserver observer) {
         this.observers.add(observer);
     }
@@ -66,81 +75,77 @@ public class ControllerSimulacao implements InterfaceControllerObserved {
     public void removeObserver(InterfaceViewObserver observer) {
         this.observers.remove(observer);
     }
-    
+
     public void carregaSimulacao(File arquivo) throws IOException {
-        BufferedReader in      = new BufferedReader(new FileReader(arquivo));
-        int            linhas  = Integer.parseInt(in.readLine());
-        int            colunas = Integer.parseInt(in.readLine());
+        BufferedReader in = new BufferedReader(new FileReader(arquivo));
+        int linhas = Integer.parseInt(in.readLine());
+        int colunas = Integer.parseInt(in.readLine());
 
         malha = new Pista[linhas][colunas];
         for (int i = 0; i < linhas; i++) {
             String row[] = in.readLine().split("\t");
             for (int j = 0; j < colunas; j++) {
                 malha[i][j] = new Pista(Integer.parseInt(row[j]));
-                if(Integer.parseInt(row[j]) == 0) {
+                if (Integer.parseInt(row[j]) == 0) {
                     malha[i][j].setCor(new Color(40, 114, 51));
                     malha[i][j].setIcone(new ImageIcon(this.getClass().getResource("/img/grama.png")));
                 } else {
                     malha[i][j].setCor(new Color(128, 128, 128));
                     malha[i][j].setIcone(new ImageIcon(this.getClass().getResource("/img/asfalto.jpg")));
-//                    if(i == 0 && j == 7){
-//                        malha[i][j].setVeiculo(new Veiculo());
-//                    }
                 }
             }
         }
-        
         this.definePistaEntradaSaida(malha);
         this.carregaMatrizPistas();
         this.notifyTableModel(new TableModelMalha(this));
-    }   
-    
+    }
+
     public void definePistaEntradaSaida(Pista[][] malha) {
         int linhas = malha.length;
         int colunas = malha[0].length;
-        
+
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
                 Pista pista = malha[i][j];
-                
+
                 pista.setIsEntrada(false);
                 pista.setIsSaida(false);
-                
-                if(i == 0) { //Está na primeira Linha
-                    if(pista.getTipo() == 3) { //Tipo 3: Estrada Baixo
+
+                if (i == 0) { //Está na primeira Linha
+                    if (pista.getTipo() == 3) { //Tipo 3: Estrada Baixo
                         pista.setIsEntrada(true);
                         this.addPistasEntrada(pista);
                     }
-                    if(pista.getTipo() == 1) { //Tipo 1: Estrada Cima
+                    if (pista.getTipo() == 1) { //Tipo 1: Estrada Cima
                         pista.setIsSaida(true);
                     }
                 }
-                
-                if(i == (linhas - 1)) { //Está na última Linha
-                    if(pista.getTipo() == 3) { //Tipo 3: Estrada Baixo
+
+                if (i == (linhas - 1)) { //Está na última Linha
+                    if (pista.getTipo() == 3) { //Tipo 3: Estrada Baixo
                         pista.setIsSaida(true);
                     }
-                    if(pista.getTipo() == 1) { //Tipo 1: Estrada Cima
+                    if (pista.getTipo() == 1) { //Tipo 1: Estrada Cima
                         pista.setIsEntrada(true);
                         this.addPistasEntrada(pista);
                     }
                 }
-                
-                if(j == 0) { //Está na primeira Coluna
-                    if(pista.getTipo() == 2) { //Tipo 2: Estrada Direita
+
+                if (j == 0) { //Está na primeira Coluna
+                    if (pista.getTipo() == 2) { //Tipo 2: Estrada Direita
                         pista.setIsEntrada(true);
                         this.addPistasEntrada(pista);
                     }
-                    if(pista.getTipo() == 4) { //Tipo 4: Estrada Esquerda
+                    if (pista.getTipo() == 4) { //Tipo 4: Estrada Esquerda
                         pista.setIsSaida(true);
                     }
                 }
-                
-                if(j == (colunas - 1)) { //Está na última Coluna
-                    if(pista.getTipo() == 2) { //Tipo 2: Estrada Direita
+
+                if (j == (colunas - 1)) { //Está na última Coluna
+                    if (pista.getTipo() == 2) { //Tipo 2: Estrada Direita
                         pista.setIsSaida(true);
                     }
-                    if(pista.getTipo() == 4) { //Tipo 4: Estrada Esquerda
+                    if (pista.getTipo() == 4) { //Tipo 4: Estrada Esquerda
                         pista.setIsEntrada(true);
                         this.addPistasEntrada(pista);
                     }
@@ -148,91 +153,100 @@ public class ControllerSimulacao implements InterfaceControllerObserved {
             }
         }
     }
-    
+
     public void iniciaSimulacao(int quantidadeVeiculo, boolean usaSemaforo, boolean usaMonitor) {
         notifyButtonChanged(true);
         new Thread(() -> {
-            Random random = new Random();
-            
-            while(this.veiculos.size() < quantidadeVeiculo) {
-                Pista pista = this.pistasEntrada.get(random.nextInt(this.pistasEntrada.size()));
+            while (start) {
+                if (this.veiculos.size() < quantidadeVeiculo) {
+                    Random random = new Random();
 
-                Veiculo veiculo = new Veiculo();
-                
-                this.addVeiculo(veiculo);
-                
-                pista.setVeiculo(veiculo);
-                veiculo.setPista(pista);
-                
-                this.notifyTableModelChanged();
-                
-                new Thread(() -> {
-                    veiculo.start();
-                }).start();
-                
-                try {
-                    Thread.sleep(veiculo.getVelocidade());
-                } catch (InterruptedException ex) {
+                    Pista pista = this.pistasEntrada.get(random.nextInt(this.pistasEntrada.size()));
 
+                    Veiculo veiculo = new Veiculo();
+
+                    this.addVeiculo(veiculo);
+                    pista.setVeiculo(veiculo);
+                    veiculo.setPista(pista);
+
+                    this.notifyTableModelChanged();
+
+                    new Thread(() -> {
+                        veiculo.start();
+                        
+                    }).start();
+                    
+                    try {
+                    Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ControllerSimulacao.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    System.out.println("passou");
                 }
+
             }
         }).start();
     }
-    
+
     public void finalizaSimulacao() {
         notifyButtonChanged(false);
     }
-    
+
+    public void notifyRemovedVehicle() {
+
+    }
+
     private void notifyTableModel(TableModelMalha tableModelMalha) {
         for (InterfaceViewObserver observer : this.observers) {
             observer.atualizaTableModel(tableModelMalha);
         }
     }
-    
+
     public synchronized void notifyTableModelChanged() {
         for (InterfaceViewObserver observer : this.observers) {
             observer.atualizaTable();
         }
     }
-    
+
     public void notifyButtonChanged(boolean iniciar) {
-        for(InterfaceViewObserver observer : this.observers) {
+        for (InterfaceViewObserver observer : this.observers) {
             observer.atualizaButton(iniciar);
         }
     }
-    
+
     @Override
     public Pista[][] getMalhaRodoviaria() {
         return this.malha;
     }
-    
-      private void carregaMatrizPistas() {
+
+    private void carregaMatrizPistas() {
         for (int x = 0; x < malha.length; x++) {
             for (int y = 0; y < malha[x].length; y++) {
                 Pista pista = malha[x][y];
-                
-                if(this.verificaIndiceExisteArray(x, y - 1)) {
+
+                if (this.verificaIndiceExisteArray(x, y - 1)) {
                     pista.setPistaEsquerda(malha[x][y - 1]);
                 }
-                
-                if(this.verificaIndiceExisteArray(x, y + 1)) {
+
+                if (this.verificaIndiceExisteArray(x, y + 1)) {
                     pista.setPistaDireita(malha[x][y + 1]);
                 }
-                
-                if(this.verificaIndiceExisteArray(x + 1, y)) {
+
+                if (this.verificaIndiceExisteArray(x + 1, y)) {
                     pista.setPistaBaixo(malha[x + 1][y]);
                 }
-                
-                if(this.verificaIndiceExisteArray(x - 1, y)) {
+
+                if (this.verificaIndiceExisteArray(x - 1, y)) {
                     pista.setPistaCima(malha[x - 1][y]);
                 }
 
             }
         }
     }
-      
+
     private boolean verificaIndiceExisteArray(int x, int y) {
         return (x >= 0 && x < this.malha.length) && (y >= 0 && y < this.malha[0].length);
     }
-    
+
 }
--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -3,27 +3,30 @@ package model;
 import controller.ControllerSimulacao;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 
 /**
  * Modelo do Veículo
+ *
  * @author Eloísa Bazzanella e Maria Eduarda Buzana
- * @since  novembro, 2021
+ * @since novembro, 2021
  */
 public class Veiculo extends Thread {
-    
+
     private ImageIcon icone;
 
-    private Pista pista;    
+    private Pista pista;
     private Pista pistaAnterior;
-    
+
     private int velocidade;
 
     private Semaphore mutex = new Semaphore(1);
-    
+
     public Veiculo() {
         this.icone = new ImageIcon(this.getClass().getResource("/img/veiculo.png"));
-        this.velocidade = 1000;
+        this.velocidade = 500;
     }
 
     public ImageIcon getIcone() {
@@ -57,72 +60,77 @@ public class Veiculo extends Thread {
     public void setVelocidade(int velocidade) {
         this.velocidade = velocidade;
     }
-    
+
     @Override
-    public void run() {
-        while(true) {
+    public synchronized void start() {
+        while (true) {
             try {
-//                this.mutex.acquire();
-                
-                if(this.pista.isSaida()) {
-                    this.pista.setVeiculo(null);
-                    ControllerSimulacao.getInstance().notifyTableModelChanged();
+                this.mutex.acquire();
+
+                if (pista.isSaida()) {
+                    this.removeVeiculo();
+                    this.mutex.release();
                     
-                    this.finalize();
+                    return;
                 }
-                
+
                 Pista proximaPista = this.getProximaPista();
-                
+
                 this.pistaAnterior = this.pista;
                 this.pistaAnterior.setVeiculo(null);
-                
+
                 this.pista = proximaPista;
                 this.pista.setVeiculo(this);
-                
-                this.mutex.acquire();
-                
+
+                this.mutex.release();
+
                 ControllerSimulacao.getInstance().notifyTableModelChanged();
-                
+
                 Thread.sleep(this.getVelocidade());
-                
-                this.mutex.release();
-            } 
-            catch (InterruptedException ex) {} 
-            catch (Throwable ex) {}
-            finally {
-                this.mutex.release();
+            } catch (InterruptedException e) {
+                System.out.println("Erro");
             }
         }
     }
-        
+
+    public void removeVeiculo() {
+        this.pista.setVeiculo(null);
+        ControllerSimulacao.getInstance().removeVeiculo(this);
+        ControllerSimulacao.getInstance().notifyTableModelChanged();
+    }
+
     public synchronized Pista getProximaPista() {
         Pista pista = null;
-        
-        switch(this.pista.getTipo()) {
-            case 1: case 5: {
+
+        switch (this.pista.getTipo()) {
+            case 1:
+            case 5: {
                 pista = this.pista.getPistaCima();
                 break;
             }
-            case 2: case 6: {
+            case 2:
+            case 6: {
                 pista = this.pista.getPistaDireita();
-                break;                
+                break;
             }
-            case 3: case 7: {
+            case 3:
+            case 7: {
                 pista = this.pista.getPistaBaixo();
-                break;  
+                break;
             }
-            case 4: case 8: {
+            case 4:
+            case 8: {
                 pista = this.pista.getPistaEsquerda();
-                break;  
+                break;
             }
             case 9: {
-                if(this.pistaAnterior.getTipo() == 11) {
+                if (this.pistaAnterior.getTipo() == 11) {
                     pista = this.pista.getPistaDireita();
                 } else {
                     Random random = new Random();
                     int opcao = random.nextInt(2);
 
-                    if(opcao == 1) {
+                    if (opcao == 1) {
                         pista = this.pista.getPistaCima();
                     } else {
                         pista = this.pista.getPistaDireita();
@@ -131,13 +139,13 @@ public class Veiculo extends Thread {
                 break;
             }
             case 10: {
-                if(this.pistaAnterior.getTipo() == 9) {
+                if (this.pistaAnterior.getTipo() == 9) {
                     pista = this.pista.getPistaCima();
                 } else {
                     Random random = new Random();
                     int opcao = random.nextInt(2);
 
-                    if(opcao == 1) {
+                    if (opcao == 1) {
                         pista = this.pista.getPistaCima();
                     } else {
                         pista = this.pista.getPistaEsquerda();
@@ -146,13 +154,13 @@ public class Veiculo extends Thread {
                 break;
             }
             case 11: {
-                if(this.pistaAnterior.getTipo() == 12) {
+                if (this.pistaAnterior.getTipo() == 12) {
                     pista = this.pista.getPistaBaixo();
                 } else {
                     Random random = new Random();
                     int opcao = random.nextInt(2);
 
-                    if(opcao == 1) {
+                    if (opcao == 1) {
                         pista = this.pista.getPistaBaixo();
                     } else {
                         pista = this.pista.getPistaDireita();
@@ -161,13 +169,13 @@ public class Veiculo extends Thread {
                 break;
             }
             case 12: {
-                if(this.pistaAnterior.getTipo() == 10) {
+                if (this.pistaAnterior.getTipo() == 10) {
                     pista = this.pista.getPistaEsquerda();
                 } else {
                     Random random = new Random();
                     int opcao = random.nextInt(2);
 
-                    if(opcao == 1) {
+                    if (opcao == 1) {
                         pista = this.pista.getPistaBaixo();
                     } else {
                         pista = this.pista.getPistaEsquerda();
@@ -175,17 +183,17 @@ public class Veiculo extends Thread {
                 }
                 break;
             }
-            
+
         }
-        
-        if(!pista.isTransitavel()) {
+
+        if (!pista.isTransitavel()) {
             pista = this.getProximaPista();
         }
-        
-        if(this.pistaAnterior != null && pista.equals(this.pistaAnterior)) {
+
+        if (this.pistaAnterior != null && pista.equals(this.pistaAnterior)) {
             pista = this.getProximaPista();
         }
-        
+
         return pista;
-    }  
+    }
 }
